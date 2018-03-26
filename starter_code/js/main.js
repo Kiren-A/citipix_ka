6ff54e1e4dad99fbd69398effef3733c2726cf45
Kiren-A <36192329+Creative-Kiren@users.noreply.github.com>
2018-03-26 18:03:14 +0100

    Corrections following feedback
    {} added after each else if
    Removed var input

--- starter_code/js/main.js
+++ starter_code/js/main.js
@@ -1,47 +1,40 @@
 $(function(){
 
 // click event listener on #submit-btn
-$("#submit-btn").on("click", function (event){
+	$("#submit-btn").on("click", function (event){
 	event.preventDefault()
 
 //store user input in var city
-var city = $("#city-type").val()
-var input = $("#city-type")
+	var city = $("#city-type").val();
+	
 
 //if input matc"hes a new york with various ways of input
-if (city === "nyc" || city === "new york" || city === "new york city"){
+	if (city === "nyc" || city === "new york" || city === "new york city"){
 //link city to url display city back-image
-$("body").attr("class", "nyc");
+	$("body").attr("class", "nyc");
 
-}
+
 
 //if input matches other various ways of input for cities
-else if (city === "san francisco" || city === "sf" || city === "bay area")
-$("body").attr("class", "sf");
+	}else if (city === "san francisco" || city === "sf" || city === "bay area"){
+	$("body").attr("class", "sf");
 
-else if (city === "los angeles" || city === "la" || city === "lax")
-$("body").attr("class", "la");
+	}else if (city === "los angeles" || city === "la" || city === "lax"){
+	$("body").attr("class", "la");
 
-else if (city === "austin" || city === "atx")
-$("body").attr("class", "austin");
+	}else if (city === "austin" || city === "atx"){
+	$("body").attr("class", "austin");
 
-else if (city === "sydney" || city === "syd")
-$("body").attr("class", "sydney");
-});
+	}else if (city === "sydney" || city === "syd")
+	$("body").attr("class", "sydney");
+	});
 
 // enter code to upper/lower case the input characters
-$("input").on("keyup", function(){
+	$("input").on("keyup", function(){
     this.value = this.value.toLowerCase();
 
-});
+	});
 
 
 
-});
-
-
-
-
-
-
-
+	});
